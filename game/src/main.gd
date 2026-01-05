a3acf2faccd569d99d7151dc0cdd4fec09e8b160
Nicholas Ochoa <nicholas.ochoa@gmail.com>
2026-01-05 17:24:33 -0600

    switch to city view for surface tools

--- game/src/main.gd
+++ game/src/main.gd
@@ -3504,10 +3504,12 @@ func _select_tool_group(index: int) -> void:
 
 
 func _auto_select_underground() -> void:
-	if city != null and ((selected_group == 4 and selected_subtool == 0) or (
-		selected_group == 7 and selected_subtool == 1
-	)) and overlay_mode != "underground":
-		_set_overlay("underground")
+	if city == null:
+		return
+	var underground_tool := (selected_group == 4 and selected_subtool == 0) or (selected_group == 7 and selected_subtool == 1)
+	var target := "underground" if underground_tool else "city"
+	if overlay_mode != target:
+		_set_overlay(target)
 
 
 func _select_subtool(index: int) -> void:
