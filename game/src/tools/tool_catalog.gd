d4b46eb47da7e3ca86e3cc7faae820cfc1a28a18
Nicholas Ochoa <nicholas.ochoa@gmail.com>
2026-01-11 18:12:36 -0600

    rename recall to cancel dispatch

--- game/src/tools/tool_catalog.gd
+++ game/src/tools/tool_catalog.gd
@@ -90,7 +90,7 @@ const GROUPS := [
 		["police", "Police", 0, 1],
 		["fire", "Fire", 0, 1],
 		["military", "Military", 0, 1],
-		["recall", "Recall All", 0, 0],
+		["recall", "Cancel Dispatch", 0, 0],
 	]},
 	{"id": "power", "name": "Power", "tools": [
 		["wires", "Power Lines", 2, 1],
