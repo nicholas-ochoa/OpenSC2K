b220e0a7f4f779de49201db844ff2a6509d513e9
Nicholas Ochoa <nicholas.ochoa@gmail.com>
2026-01-24 19:29:01 -0600

    fix

--- game/tests/newspaper_layout_test.gd
+++ game/tests/newspaper_layout_test.gd
@@ -12,6 +12,7 @@ func _run() -> void:
 			for b in range(a+1,rects.size()):
 				assert(not rects[a].intersects(rects[b]))
 		page.set_page(layout,'New City Journal','Sunday 4 January 1900','One Cent','Survey\nTrouble Getting Around','Weather Report\nChilly Weather',['Crowd Celebrates New City Founding','New City Founded','Inhabitants Cannot Get Around','Brownouts Cost','More Power To Us'])
+		page.set_picture(0, null)
 		page.set_articles(['The new city is ready. '.repeat(100),'Residents celebrate their new city. '.repeat(150),'The city needs transport. '.repeat(100),'More electricity is needed. '.repeat(100),'Power reaches more homes. '.repeat(100)])
 		assert(not page.article_labels[1].text.is_empty())
 		assert(page.article_labels[1].horizontal_alignment == HORIZONTAL_ALIGNMENT_FILL)
