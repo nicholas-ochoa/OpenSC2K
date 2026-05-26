07a83e142f90e86761d5385dd4d5674f676f3186
Nicholas Ochoa <nicholas.ochoa@gmail.com>
2026-05-26 09:23:20 -0500

    extract disaster map constants

--- game/src/simulation/disasters/disaster_map_phase.gd
+++ game/src/simulation/disasters/disaster_map_phase.gd
@@ -1,45 +1,6 @@
 class_name DisasterMapPhase
-extends RefCounted
+extends DisasterMapConstants
 
-const DisasterMapDamage = preload("res://src/simulation/disasters/disaster_damage.gd")
-const Demolish = preload("res://src/tools/city/demolish_command.gd")
-const Growth = preload("res://src/simulation/growth/growth_phase.gd")
-const NetworkTiles = preload("res://src/tools/city/network_command.gd")
-const FIRE_OVERLAY := 0xff
-const TOXIC_OVERLAY := 0xfb
-const FLOOD_OVERLAY := 0xfc
-const RIOT_OVERLAY_FORWARD := 0xfd
-const RIOT_OVERLAY_REVERSE := 0xfe
-const SOUND_FIRE := 0x1fb
-const SOUND_FLOOD := 0x1ff
-const SOUND_RIOT := 0x200
-const SOUND_HURRICANE := 0x1f6
-const SOUND_EARTHQUAKE := 0x1f8
-const TYPE_EXPLOSION := 6
-const TYPE_POLICE := 7
-const TYPE_FIRE_DISPATCH := 8
-const TYPE_MILITARY := 14
-const TEXT_THING_BASE := 201
-const SPECIAL_TOXIC_BUILDINGS := {0x85: true, 0x9f: true, 0xbc: true}
-const CARDINAL_DIRECTIONS := [
-	Vector2i(-1, 0), Vector2i(0, -1), Vector2i(1, 0), Vector2i(0, 1),
-]
-const MAP_CHUNK_SIZES := {
-	"ALTM": CityState.TILE_COUNT * 2,
-	"XBLD": CityState.TILE_COUNT,
-	"XTER": CityState.TILE_COUNT,
-	"XZON": CityState.TILE_COUNT,
-	"XUND": CityState.TILE_COUNT,
-	"XBIT": CityState.TILE_COUNT,
-	"XTRF": 64 * 64,
-	"XVAL": 64 * 64,
-	"XTXT": CityState.TILE_COUNT,
-	"XLAB": CityState.LABEL_COUNT * CityState.LABEL_RECORD_SIZE,
-	"XMIC": CityState.MICROSIM_COUNT * CityState.MICROSIM_RECORD_SIZE,
-	"XTHG": CityState.THING_COUNT * CityState.THING_RECORD_SIZE,
-	"XFIR": 32 * 32,
-	"MISC": 4800,
-}
 
 
 static func run_all(
