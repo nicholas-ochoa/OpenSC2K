8271eccc585c706fd7f459f1c221b9fdfd49799c
Nicholas Ochoa <nicholas.ochoa@gmail.com>
2026-06-14 07:11:16 -0500

    add annual facility context

---
+++ game/src/simulation/civic/annual/context.gd
@@ -0,0 +1,45 @@
+class_name MicrosimAnnualContext
+extends RefCounted
+
+
+var city: CityState
+var bus_passengers: int
+var rail_passengers: int
+var subway_passengers: int
+var random: Variant
+var lfsr_random: Variant
+var game_random: Variant
+var power_usage_percent: int
+var water_usage_percent: int
+var australian_locale: bool
+var mayor_approval: int
+var map_edge: int
+var span: SimulationTimingSpan
+var old_payloads: Dictionary
+var changed_payloads: Dictionary
+var microsims: PackedByteArray
+var misc: PackedByteArray
+var subway_count: int
+var bus_count: int
+var rail_count: int
+var counts: Dictionary
+var old_arrests: int
+var prison_population: int
+var prison_count: int
+var news_items: Array
+var random_records_pending: int
+var low_school_score: bool
+var expired_power_records: Array
+var demolished_power_records: Array
+var arcology_population: int
+var arcology_launch_pending: bool
+var launch_arcology_records: int
+var launched_structures: int
+var arcology_launched: bool
+var sound_events: Array
+var view_center_requests: Array
+var effect_events: Array[Dictionary]
+var next_effect_frame: int
+var updated_subway: int
+var updated_bus: int
+var updated_rail: int
