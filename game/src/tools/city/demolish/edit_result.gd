ae2be845b802470c859717e24c9c27073180121d
Nicholas Ochoa <nicholas.ochoa@gmail.com>
2026-08-04 21:10:56 -0500

    add edit edit result

---
+++ game/src/tools/city/demolish/edit_result.gd
@@ -0,0 +1,30 @@
+class_name DemolishEditResult
+extends EditCommandResult
+
+
+var underground_view := false
+var scurk_mode := false
+var action_count := 0
+
+var skipped_specialized := 0
+var skipped_insufficient := 0
+# forest protests keep the tree and add a saved news story
+var easter_events := 0
+var news_items: Array[Dictionary] = []
+var news_queue_updated := false
+
+
+static func rejected(message: String, charged := 0) -> DemolishEditResult:
+	var result := DemolishEditResult.new()
+	result.error = message
+	result.cost = charged
+
+	return result
+
+
+func _merge_counts(stroke: EditCommandResult) -> void:
+	var demolition := stroke as DemolishEditResult
+	action_count += demolition.action_count
+	easter_events += demolition.easter_events
+	skipped_specialized += demolition.skipped_specialized
+	skipped_insufficient += demolition.skipped_insufficient
