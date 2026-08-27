2f7cbeae0e2116e3f5e277561df7da76205a4a66
Nicholas Ochoa <nicholas.ochoa@gmail.com>
2026-08-27 18:18:25 -0500

    add asset and simulation disaster start result

---
+++ game/src/simulation/disasters/disaster_start_result.gd
@@ -0,0 +1,42 @@
+class_name DisasterStartResult
+extends PhaseResult
+
+
+class MaxisManArrival extends RefCounted:
+	var record := 0
+	var point := Vector2i.ZERO
+	var target := Vector2i.ZERO
+	var goal := 0
+
+
+var disaster_type := 0
+var point := Vector2i.ZERO
+var started := false
+var implemented := false
+var record := 0
+var notice_ids: Array[int] = []
+var map_counter := 0
+var hurricane_counter := 0
+var map_changed := false
+var plant_point := Vector2i.ZERO
+var plant_site := Rect2i()
+var path_finish := Vector2i.ZERO
+var requested_point := Vector2i.ZERO
+var scan_finish := Vector2i.ZERO
+var direction := 0
+var terrain_indices := PackedInt32Array()
+var result_codes := PackedInt32Array()
+var seed_points: Array[Vector2i] = []
+var candidate_points: Array[Vector2i] = []
+var damage_points: Array[Vector2i] = []
+var flood_points: Array[Vector2i] = []
+var accepted_points: Array[Vector2i] = []
+var counters: Dictionary[String, int] = {}
+var maxis_man_response: MaxisManArrival
+
+
+static func failed(message: String) -> DisasterStartResult:
+	var result := DisasterStartResult.new()
+	result.error = message
+
+	return result
