97f23b2ad93c31b3a0d78dd11b2e3bf9c0eac0c9
Nicholas Ochoa <nicholas.ochoa@gmail.com>
2026-08-27 14:35:45 -0500

    add asset and simulation asset import result

---
+++ game/src/assets/asset_import_result.gd
@@ -0,0 +1,23 @@
+class_name AssetImportResult
+extends RefCounted
+
+
+var ok := false
+var error := ""
+var path := ""
+var root := ""
+var hash := ""
+var previous_root := ""
+var created := PackedStringArray()
+var cities := 0
+var scenarios := 0
+var graphics := ""
+var sound := ""
+var music := ""
+
+
+static func failure(message: String) -> AssetImportResult:
+	var result := AssetImportResult.new()
+	result.error = message
+
+	return result
