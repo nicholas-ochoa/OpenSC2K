75aab1240b282523a95905cd96c6f2fbe880a993
Nicholas Ochoa <nicholas.ochoa@gmail.com>
2026-09-01 09:23:20 -0500

    add simulation and demolition point result

---
+++ game/src/tools/city/demolish/point_result.gd
@@ -0,0 +1,10 @@
+class_name DemolishPointResult
+extends RefCounted
+
+
+var changed := false
+var error := ""
+var specialized := false
+var easter_event := false
+var indices := PackedInt32Array()
+var effect_events: Array[Dictionary] = []
